4f6066d1a7db34f85613ab3412f5d04527742cf8
Ameya Santosh Gidh <65457905+ameyagidh@users.noreply.github.com>
2023-05-06 17:55:01 -0400

    PythonForEverybody

---
+++ main.py
@@ -0,0 +1,21 @@
+# Python for every body
+# https://github.com/ameyagidh/MYOnlineCoursesAssignmentMain.git
+# Token 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
+# /Users/ameyagidh/AAAmeya/AllWork/python
+
+# week1
+# print("hello world")
+
+# week2
+# P1
+# a = input("Enter your name \n")
+# print("Hello",a)
+
+# P2
+# hrs = input("Enter hrs \n")
+# rate = input("enter rate \n")
+# print(float(hrs)*float(rate))
+
+# week3
+
+
