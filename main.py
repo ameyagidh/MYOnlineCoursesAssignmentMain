1521966081dacd515a20ff1e8db2b26562bc24ac
Ameya Santosh Gidh <65457905+ameyagidh@users.noreply.github.com>
2023-05-06 22:24:53 -0400

    p7

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Token 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
 # /Users/ameyagidh/AAAmeya/AllWork/PythonForEverybody
 
+# Course 1
+
 # week1
 # print("hello world")
 
@@ -29,6 +31,77 @@
 
 # week 5
 # P1
+# ans = []
+# while (True):
+#     res = input("Enter a number or done to exit \n")
+#     if res == "done":
+#         break
+#     try:
+#         res.isdigit()
+#         ans.append(res)
+#         largest = max(ans)
+#         smallest = min(ans)
+#         print("Largest ",largest)
+#         print("Smallest ", smallest)
+#     except:
+#         raise "not a digit"
+# print("Done")
+
+# ------------------------------------------
+
+# Course 2
+
+# P1
+# text = "X-DSPAM-Confidence:    0.8475"
+# import re
+# res = re.findall(r"\d+",text)
+# res2 = ".".join(res)
+# print(res2)
+
+# text = "X-DSPAM-Confidence:    0.8475"
+# res = text.find(":")
+# print(text[res+1:].strip())
+
+# P2
+# fname = input("Enter file name \n")
+# fname = "words.txt"
+# fh = open(fname,"r")
+# f = fh.read()
+# print(f.upper().rstrip())
+# for line in fh:
+#     print(line.upper())
+# print(fh.read())
+
+# P3
+# fname = "mbox-short.txt"
+# fh = open(fname,"r")
+# # f = fh.read()
+# res = []
+# total = 0
+# ctr = 0
+# for line in fh:
+#     if not line.startswith("X-DSPAM-Confidence:"):
+#         continue
+#     idx = line.find(":")
+#     ctr += 1
+#     total += float(line[idx+1:].strip())
+# print(total/ctr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
